2ae7b8044c2b2ce6ed590a6ec777fa2c1c84c80f
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-10-23 18:13:33 +0100

    still configuring my models

--- blog_zona/models.py
+++ blog_zona/models.py
@@ -12,8 +12,20 @@ class Category(models.Model):
 
 
 class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options  = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     title = models.CharField(max_length=250)
     excerpt = models.TextField(null=True)
     content = models.TextField()
     slug = models.SlugField(max_length=250, unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(max_length=10, choices=options,default='published')
