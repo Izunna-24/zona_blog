680819a977c93fd8e7de64f89641169e7a2d0936
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-10-23 17:29:12 +0100

    models modified

--- blog_zona/models.py
+++ blog_zona/models.py
@@ -9,3 +9,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published')
